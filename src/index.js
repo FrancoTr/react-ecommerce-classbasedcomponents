d2fe7c7dc2c8ac8fe9423e125cd19e867355f69e
Franco Traverso <franco-t20@outlook.com>
2021-11-09 23:13:09 -0300

    import jquery & bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "jquery";
+import "bootstrap/dist/js/bootstrap";
 
 var element = <div>Class based components</div>;
 ReactDOM.render(element, document.getElementById("root"));
