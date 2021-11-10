1a1608435f3c9d224477b3380ffa2573fb3868ca
Franco Traverso <franco-t20@outlook.com>
2021-11-10 19:53:19 -0300

    add global css file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./App";
+import "index.css";
 
 var element = <button class="btn btn-danger">Class based components</button>;
 ReactDOM.render(element, document.getElementById("root"));
