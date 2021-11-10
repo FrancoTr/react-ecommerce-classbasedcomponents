3f54e9a8ddc1ccdc41922659694a055eec2930c5
Franco Traverso <franco-t20@outlook.com>
2021-11-09 23:14:39 -0300

    import popper.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "jquery";
+import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 
 var element = <div>Class based components</div>;
