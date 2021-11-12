bc716b7b495d039f26eb6f1ab5c83ffaef3b9b35
Franco Traverso <franco-t20@outlook.com>
2021-11-12 20:45:47 -0300

    add state to MainContent component

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
 export default class MainContent extends Component {
+  state = { appTitle: "Customers" };
+
   render() {
-    return <div>Main Component</div>;
+    return (
+      <div>
+        <h4>{this.state.appTitle}</h4>
+      </div>
+    );
   }
 }
