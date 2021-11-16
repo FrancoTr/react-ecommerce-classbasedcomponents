4ec7b41d99a6ea9522b7f8befef67f064f7d01c7
Franco Traverso <franco-t20@outlook.com>
2021-11-15 22:18:41 -0300

    add warning background to the No Phone label when there is no available phone number

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -37,7 +37,9 @@ export default class MainContent extends Component {
                 <tr key={cust.id}>
                   <td>{cust.id}</td>
                   <td>{cust.name}</td>
-                  <td>{cust.phone == null ? "No Phone" : cust.phone}</td>
+                  <td>
+                    {cust.phone ? cust.phone : <div className="bg-warning p-2">No Phone</div>}
+                  </td>
                   <td>{cust.address.city}</td>
                 </tr>
               );
