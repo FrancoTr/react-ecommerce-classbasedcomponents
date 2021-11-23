41d4df2ee307ed8f0b86ad6e29c0fbe06c6b0d05
Franco Traverso <franco-t20@outlook.com>
2021-11-23 19:53:16 -0300

    add Buy Now button to Product component

--- src/Product.jsx
+++ src/Product.jsx
@@ -14,6 +14,7 @@ export default class Product extends Component {
             <h5 className="pt-2 border-top">{this.state.product.productName}</h5>
             <div>${this.state.product.price}</div>
           </div>
+          <div className="card-footer text-right">{this.props.children}</div>
         </div>
       </div>
     );
