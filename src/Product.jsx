45c2369b32b8336494370dbcde02d931c00e124e
Franco Traverso <franco-t20@outlook.com>
2021-11-23 18:46:31 -0300

    fix typo on bootstrap class

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Product extends Component {
   render() {
     return (
-      <div className="col-lg 6">
+      <div className="col-lg-6">
         <div className="card m-2">
           <div className="card-body">
             <div className="text-muted"># {this.props.id}</div>
