e551a86f8315de150d70d0bcb9d7075354025cf7
Franco Traverso <franco-t20@outlook.com>
2021-11-25 23:07:44 -0300

    add quantity field and quantity buttons to Product component

--- src/Product.jsx
+++ src/Product.jsx
@@ -14,7 +14,16 @@ export default class Product extends Component {
             <h5 className="pt-2 border-top">{this.state.product.productName}</h5>
             <div>${this.state.product.price}</div>
           </div>
-          <div className="card-footer text-right">{this.props.children}</div>
+          <div className="card-footer">
+            <div className="float-left">
+              <span className="badge">{this.state.product.quantity}</span>
+              <div className="btn-group">
+                <button className="btn btn-outline-success">+</button>
+                <button className="btn btn-outline-success">-</button>
+              </div>
+            </div>
+            <div className="float-right">{this.props.children}</div>
+          </div>
         </div>
       </div>
     );
